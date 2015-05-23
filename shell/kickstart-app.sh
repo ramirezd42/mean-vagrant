23ae882ac35e926bf879ff97a27b72d1b6b237a5
Dave <Dave@dave-laptop>
2015-05-23 19:37:55 -0400

    Removed a lot of app initialization scripting to make reprovisioning easier once development has begun

--- shell/kickstart-app.sh
+++ shell/kickstart-app.sh
@@ -11,7 +11,6 @@ if [ -d "$1" ]; then
 else
 
 	#from here let's now create a new application
-	express $1
 	cd $1
 
 	#be a good citizen and add in the mongo dep in the package.json file for your application
@@ -21,35 +20,8 @@ else
 	#run an npm link to all the global packages we installed
 	npm link
 
-	#create a special bower file to tell bower where to put vendor javascript files
-	#files will be under public/javascripts/vendor
-	echo '{ "directory" : "public/javascripts/vendor" }' >> .bowerrc
-
-	mkdir public/javascripts/vendor
-
-	#install angular 
-	bower install angular --allow-root
-
-	#install bootstrap-css
-	bower install bootstrap-css --allow-root
-
-	#install bootstrap-javascript
-	bower install bootstrap-javascript --allow-root
-
-	#install angular-bootstrap
-	bower install angular-bootstrap --allow-root
-
-	#install angular-scenario for testing
-	bower install angular-scenario --allow-root
-
 	#finally change the owner of everything to meandev user and group
 	cd $MEAN_DIR
 
 	chown -R meandev:meandev $1	
-
-	#change to the app directory
-	cd $1 
-
-	#start express server
-	DEBUG=$1:* ./bin/www
 fi
